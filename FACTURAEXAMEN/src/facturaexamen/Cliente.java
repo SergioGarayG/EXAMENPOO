4cf420a8c5bbe4cbc3ca3f11028f5a0fdf12fb66
Sergio <sergio7garay@gmail.com>
2020-05-23 17:56:07 -0600

    ACTUALIZACIONO.111

---
+++ FACTURAEXAMEN/src/facturaexamen/Cliente.java
@@ -0,0 +1,14 @@
+
+package facturaexamen;
+
+public class Cliente {
+    private Boolean CreditoActivo;
+
+    public Boolean getCreditoActivo() {
+        return CreditoActivo;
+    }
+
+    public void setCreditoActivo(Boolean CreditoActivo) {
+        this.CreditoActivo = CreditoActivo;
+    }
+}
