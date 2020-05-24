a00bd1cc5912fbb6ba81966a01899d6f4f188f94
said199 <61359272+said199@users.noreply.github.com>
2020-05-23 20:54:24 -0600

    Add files via upload
    actualizacion0.113

---
+++ EXAMENPOO-d8b3b50ac53dd3d617390a77d325606650b35493/FACTURAEXAMEN/src/facturaexamen/Vendedor.java
@@ -0,0 +1,15 @@
+
+package facturaexamen;
+
+
+public class Vendedor {
+    private String Sueldo;
+
+    public String getSueldo() {
+        return Sueldo;
+    }
+
+    public void setSueldo(String Sueldo) {
+        this.Sueldo = Sueldo;
+    }
+}
